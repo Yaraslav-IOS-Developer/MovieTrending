d86bf550102cbfe4152b118ce0d460735f0cfbb1
Yaroslav <yaroslav.derugo@mobyrix.com>
2023-08-06 13:18:13 +0300

    Create TrendingTv model.

---
+++ MovieTrending/Extensions/Foundation/String/String+Extensions.swift
@@ -0,0 +1,18 @@
+//
+//  String+Extensions.swift
+//  MovieTrending
+//
+//  Created by Yaroslav on 6.08.23.
+//
+
+import Foundation
+
+
+extension String {
+  func capitalizedFirst() -> String {
+    guard let firstChar = first else {
+      return self
+    }
+    return String(firstChar).uppercased() + dropFirst()
+  }
+}
